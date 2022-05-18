28b22c2971a7180cbed88d03d94226ddf71a44b3
vipulkr059 <vipulkr059@gmail.com>
2022-05-18 17:24:22 +0530

    make it better

--- routes/application.js
+++ routes/application.js
@@ -1,19 +1,17 @@
 var express = require("express");
 var router = express.Router();
 const { check } = require("express-validator");
-const { apply , getUserById} = require("../controllers/application");
-const { isLogin, isAuthenticated, isAuthority } = require("../controllers/auth");
-
+const { apply, getUserById } = require("../controllers/application");
+const {
+  isLogin,
+  isAuthenticated,
+  isAuthority,
+} = require("../controllers/auth");
 
 //Get User By Id Parameter
 router.param("userId", getUserById);
 
 //Submitting Form, updating user details
-router.put(
-    "/application/:userId",
-    isLogin,
-    isAuthenticated,
-    apply
-  );
-  
+router.put("/application/:userId", isLogin, isAuthenticated, apply);
+
 module.exports = router;

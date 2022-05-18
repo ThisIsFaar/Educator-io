237e27d7fd9e5a0cd41015cdb253c2510359e4c9
thisisfaar <thisisfarhan99@gmail.com>
2022-05-18 22:20:58 +0530

    serve fixed

--- app.js
+++ app.js
@@ -17,15 +17,12 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-// Accessing the path module
-const path = require("path");
-
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+if (process.env.NODE_ENV === `production` || process.env.NODE_ENV === `staging`) {
+  app.use(express.static(`client/build`));
+  app.get(`*`, (req, res) => {
+  res.sendFile(path.join(__dirname + `/client/build/index.html`));
+  });
+ }
 
 const authRoutes = require("./routes/auth");
 const application  = require("./routes/application");

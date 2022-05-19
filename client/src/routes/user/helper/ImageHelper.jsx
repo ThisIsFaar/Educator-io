59b2e0f1e93048b5e3358cf88b2751ed08c07cee
thisisfaar <thisisfarhan99@gmail.com>
2022-05-20 00:59:06 +0530

    retake

--- client/src/routes/user/helper/ImageHelper.jsx
+++ client/src/routes/user/helper/ImageHelper.jsx
@@ -7,7 +7,7 @@ const ImageHelper = ({ user }) => {
     ? `${API}/application/photo/${user._id}`
     : `https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`;
   return (
-    <img src={imageUrl} alt="photo" className="rounded-circle w-100 h-100"  style={{borderRadius: "15rem"}}/>
+    <img src={imageUrl} alt="photo" className="rounded-circle w-100 h-100" />
   );
 };
 

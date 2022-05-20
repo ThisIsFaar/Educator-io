80ccd9b10c41ae53b84a06975b4a4439af0aa194
thisisfaar <thisisfarhan99@gmail.com>
2022-05-20 21:27:47 +0530

    logout added

--- client/src/auth/helper/index.js
+++ client/src/auth/helper/index.js
@@ -86,12 +86,6 @@ export const authenticate = (data, next) => {
 export const logout = (next) => {
   if (typeof window !== "undefined") {
     localStorage.removeItem("jwt");
-    next();
-    return fetch(`${API}/signout`, {
-      method: "GET",
-    })
-      .then((response) => console.log("signout success"))
-      .catch((err) => console.log(err));
   }
 };
 

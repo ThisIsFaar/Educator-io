68cb5459b556e5cf3dd8da569d24e941b5e5e227
thisisfaar <thisisfarhan99@gmail.com>
2022-05-20 21:37:39 +0530

    prompt added

--- client/src/routes/authority/UpdateReqModal.jsx
+++ client/src/routes/authority/UpdateReqModal.jsx
@@ -8,66 +8,88 @@ import "react-toastify/dist/ReactToastify.css";
 import { acceptUpdateReq, rejectUpdateReq } from "./helper";
 import { isAuthenticated } from "../../auth/helper";
 
-
 export default function UpdateReqModal({
   muser,
   modal,
   setmodal,
   onClose,
   updateData,
-}) 
-
-
-
-{
-const { user, token } = isAuthenticated();
-  let data ={};
-  if (updateData.phoneNumber !== "" ) {
-    data.phoneNumber = updateData.phoneNumber
+}) {
+  const { user, token } = isAuthenticated();
+  let data = {};
+  if (updateData.phoneNumber !== "") {
+    data.phoneNumber = updateData.phoneNumber;
   }
-  if (updateData.dateOfJoining !== "" ) {
-    data.dateOfJoining = updateData.dateOfJoining
+  if (updateData.dateOfJoining !== "") {
+    data.dateOfJoining = updateData.dateOfJoining;
   }
-  if (updateData.postedDesignation !== "" ) {
-    data.postedDesignation = updateData.postedDesignation
+  if (updateData.postedDesignation !== "") {
+    data.postedDesignation = updateData.postedDesignation;
   }
-  if (updateData.postedSchoolLocation !== "" ) {
-    data.postedSchoolLocation = updateData.postedSchoolLocation
+  if (updateData.postedSchoolLocation !== "") {
+    data.postedSchoolLocation = updateData.postedSchoolLocation;
   }
-  if (updateData.postedSchoolName !== "" ) {
-    data.postedSchoolName = updateData.postedSchoolName
+  if (updateData.postedSchoolName !== "") {
+    data.postedSchoolName = updateData.postedSchoolName;
   }
-  if (updateData.address !== "" ) {
-    data.address = updateData.address
+  if (updateData.address !== "") {
+    data.address = updateData.address;
   }
-  data.userId = muser._id
+  data.userId = muser._id;
 
   // user.data= data;
   const onAccept = () => {
     acceptUpdateReq(user, token, muser, data)
-    .then((data) => {
+      .then((data) => {
         if (data.status === 200) {
-          console.log(data.message);
+          toast.success(data.message, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
         } else if (data.status === 400) {
+          toast.error(data.message, {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            });
         }
       })
-      .catch((err) => console.log(err))
-  }  
+      .catch((err) => console.log(err));
+  };
 
   const onReject = (user) => {
     rejectUpdateReq(user, data)
-    .then((data) => {
+      .then((data) => {
         if (data.status === 200) {
           console.log(data.message);
         } else if (data.status === 400) {
         }
       })
-      .catch((err) => console.log(err))
-  }  
+      .catch((err) => console.log(err));
+  };
 
   return (
-    
     <div className={"modal " + (modal && "active")}>
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="wrapper" style={{ width: "30vw" }}>
         <div
           className="left-container"
@@ -77,33 +99,55 @@ const { user, token } = isAuthenticated();
             <ImageHelper user={muser} />
           </div>
           <div className="info">
-            <div className="updateinfo" >
+            <div className="updateinfo">
               <div style={{ fontSize: "3rem" }}>Update Details</div>
               <div>
-              <span>Phone</span>: <span>{updateData.phoneNumber}</span>
+                <span>Phone</span>: <span>{updateData.phoneNumber}</span>
               </div>
 
               <div>
-              <span>Address</span>: <span>{updateData.address ? updateData.address : "No Updates Required"}</span>
-              </div>
-
-              
-              <div>
-              <span>School Name</span>: <span>{updateData.postedSchoolName ? updateData.postedSchoolName : "No Updates Required"}</span>
-              </div>
-              <div>
-              <span>School Location</span>:{" "}
-              <span>{updateData.postedSchoolLocation ? updateData.postedSchoolLocation : "No Updates Required"}</span>
+                <span>Address</span>:{" "}
+                <span>
+                  {updateData.address
+                    ? updateData.address
+                    : "No Updates Required"}
+                </span>
               </div>
 
               <div>
-              <span>Posted Designation</span>:{" "}
-              <span>{updateData.postedDesignation ? updateData.postedDesignation : "No Updates Required"}</span>
+                <span>School Name</span>:{" "}
+                <span>
+                  {updateData.postedSchoolName
+                    ? updateData.postedSchoolName
+                    : "No Updates Required"}
+                </span>
+              </div>
+              <div>
+                <span>School Location</span>:{" "}
+                <span>
+                  {updateData.postedSchoolLocation
+                    ? updateData.postedSchoolLocation
+                    : "No Updates Required"}
+                </span>
               </div>
 
-              <div><span>Date Of Joining</span>: <span>{
-                updateData.dateOfJoining ? updateData.dateOfJoining : "No Updates Required"
-                }</span></div>
+              <div>
+                <span>Posted Designation</span>:{" "}
+                <span>
+                  {updateData.postedDesignation
+                    ? updateData.postedDesignation
+                    : "No Updates Required"}
+                </span>
+              </div>
+
+              <div>
+                <span>Date Of Joining</span>:{" "}
+                <span>
+                  {updateData.dateOfJoining
+                    ? updateData.dateOfJoining
+                    : "No Updates Required"}
+                </span>
+              </div>
             </div>
           </div>
           <div

9ca86eb81f0b375dc3e0bf02e9a22f328d73d359
vipulkr059 <vipulkr059@gmail.com>
2022-05-23 00:19:33 +0530

    Update Modal Image Fixed

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
-const path = require('path')
+const path = require("path");
 
 mongoose
-  .connect("mongodb+srv://farhan:0987@cluster0.satq4.mongodb.net/?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://farhan:0987@cluster0.satq4.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => console.log("Database connected ✅"))
   .catch((err) => console.log("Database connectivity problem occur", err));
 
@@ -22,21 +24,21 @@ app.use(cors());
 // app.get(`*`, (req, res) => {
 // res.sendFile(path.join(__dirname + `/client/build/index.html`));
 // });
-app.use('/', express.static(path.join(__dirname, '/client/build')));
-
+app.use("/", express.static(path.join(__dirname, "/client/build")));
 
 const authRoutes = require("./routes/auth");
-const application  = require("./routes/application");
+const application = require("./routes/application");
 
 app.use("/api", authRoutes);
 app.use("/api", application);
 
-// error handling for express-jwt authentication
-// app.use( (err, req, res, next) =>  {
-//   if (err.name === 'UnauthorizedError') {
-//     res.status(401).send('invalid token...');
-//   }
-// });
+//error handling for express-jwt authentication
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    console.log("We Are Here");
+    res.status(401).send("invalid token...");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Backend is running at port number ${port} ✅`);

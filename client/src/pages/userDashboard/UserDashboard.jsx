3260c0a2e8f9252cdff5d02eff83f169acd74821
Nitesh Sethi <nonusethi1272@gmail.com>
2023-01-17 02:00:24 +0530

    Changed UI of application form.

--- client/src/pages/userDashboard/UserDashboard.jsx
+++ client/src/pages/userDashboard/UserDashboard.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 // import './userDashboard.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faBars, faXmark
-} from '@fortawesome/free-solid-svg-icons';
-import { sideBarData } from './data';
-import Application from './pages/Application';
-import Profile from './pages//Profile';
-import UpdateRequest from './pages/Updaterequest';
-import classNames from 'classnames';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { sideBarData } from "./data";
+import Application from "./pages/Application";
+import Profile from "./pages//Profile";
+import UpdateRequest from "./pages/Updaterequest";
+import classNames from "classnames";
 
 export default function UserDashboard() {
-  const [page, setpage] = useState('Application');
+  const [page, setpage] = useState("Application");
   const [menuToggle, SetMenuToggle] = useState(false);
   return (
     // <div className="mainContainer bg-secondary">
@@ -43,13 +41,11 @@ export default function UserDashboard() {
     //   </div>
     // </div>
 
-
     <div className="flex flex-no-wrap">
       {/* Sidebar starts */}
       {/* Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] */}
       <div className="w-64 absolute sm:relative bg-slate-900 shadow md:h-screen flex-col justify-between hidden sm:flex">
         <div className="px-8">
-
           <ul className="mt-12">
             {sideBarData.map((item, key) => {
               return (
@@ -62,10 +58,12 @@ export default function UserDashboard() {
                     <FontAwesomeIcon
                       className="icon icon-tabler icon-tabler-compass"
                       icon={item.icon}
-                    // color="#a4a6b3"
-                    // size="2x"
+                      // color="#a4a6b3"
+                      // size="2x"
                     />
-                    <span className="text-sm uppercase text-center font-semibold ml-2">{item.title}</span>
+                    <span className="text-sm uppercase text-center font-semibold ml-2">
+                      {item.title}
+                    </span>
                   </div>
                 </li>
               );
@@ -73,23 +71,28 @@ export default function UserDashboard() {
           </ul>
         </div>
       </div>
-      <div className={classNames("w-64 h-screen z-40 absolute bg-gray-800 shadow md:h-full flex-col justify-between sm:hidden  transition duration-150 ease-in-out", {
-        "translate-x-[0px]": menuToggle
-      },
-        {
-          "translate-x-[-260px]": !menuToggle
-        }
-
-      )} id="mobile-nav">
-        <div className="h-10 w-10 bg-gray-800 absolute right-0 mt-16 -mr-10 flex items-center shadow rounded-tr rounded-br text-secondary justify-center cursor-pointer" id="mobile-toggler"
-          onClick={() => SetMenuToggle(!menuToggle)}>
-
+      <div
+        className={classNames(
+          "w-64 h-screen z-40 absolute bg-gray-800 shadow md:h-full flex-col justify-between sm:hidden  transition duration-150 ease-in-out",
           {
-            menuToggle ?
-              <FontAwesomeIcon icon={faXmark} />
-              :
-              <FontAwesomeIcon icon={faBars} />
+            "translate-x-[0px]": menuToggle,
+          },
+          {
+            "translate-x-[-260px]": !menuToggle,
           }
+        )}
+        id="mobile-nav"
+      >
+        <div
+          className="h-10 w-10 bg-gray-800 absolute right-0 mt-16 -mr-10 flex items-center shadow rounded-tr rounded-br text-secondary justify-center cursor-pointer"
+          id="mobile-toggler"
+          onClick={() => SetMenuToggle(!menuToggle)}
+        >
+          {menuToggle ? (
+            <FontAwesomeIcon icon={faXmark} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
         </div>
         <div className="px-8">
           <ul className="mt-12">
@@ -98,16 +101,21 @@ export default function UserDashboard() {
                 <li
                   key={key}
                   class="flex w-full justify-between text-secondary hover:text-white cursor-pointer items-center mb-6"
-                  onClick={() => { setpage(item.title); SetMenuToggle(!menuToggle) }}
+                  onClick={() => {
+                    setpage(item.title);
+                    SetMenuToggle(!menuToggle);
+                  }}
                 >
                   <div className="flex justify-center items-center">
                     <FontAwesomeIcon
                       className="icon icon-tabler icon-tabler-compass"
                       icon={item.icon}
-                    // color="#a4a6b3"
-                    // size="2x"
+                      // color="#a4a6b3"
+                      // size="2x"
                     />
-                    <span className="text-sm uppercase text-center font-semibold ml-2">{item.title}</span>
+                    <span className="text-sm uppercase text-center font-semibold ml-2">
+                      {item.title}
+                    </span>
                   </div>
                 </li>
               );
@@ -116,15 +124,14 @@ export default function UserDashboard() {
         </div>
       </div>
       {/* Sidebar ends */}
-      {/* Remove class [ h-64 ] when adding a card block */}
-      <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6 h-screen">
-        {/* Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border */}
-        <div className="w-full h-full rounded border-dashed border-2 border-gray-300">{/* Place your content here */}
-          {page === 'Application' && <Application />}
-          {page === 'User Profile' && <Profile />}
-          {page === 'Update Request' && <UpdateRequest />}
+      <div className="container mx-auto py-10 md:w-4/5 w-11/12 px-6 h-screen">
+        <div className="w-full h-full">
+          {/* Place your content here */}
+          {page === "Application" && <Application />}
+          {page === "User Profile" && <Profile />}
+          {page === "Update Request" && <UpdateRequest />}
         </div>
       </div>
     </div>
-  )
+  );
 }

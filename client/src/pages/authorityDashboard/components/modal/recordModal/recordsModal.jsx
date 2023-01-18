35b8595ad64d69e395aedae7280b600568d72c0a
Nitesh Sethi <nonusethi1272@gmail.com>
2023-01-19 00:28:21 +0530

    Changed UI of Teachers Reacord.

--- client/src/pages/authorityDashboard/components/modal/recordModal/recordsModal.jsx
+++ client/src/pages/authorityDashboard/components/modal/recordModal/recordsModal.jsx
@@ -1,77 +1,179 @@
-import React from 'react';
-import './recordModal.css';
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ImageHelper from '../../../../../component/ImageHelper';
+import React from "react";
+import "./recordModal.css";
+import ImageHelper from "../../../../../component/ImageHelper";
 
 export default function RecordsModal({ user, modal, setmodal }) {
   return (
     <div
-      className={'Recordsmodal ' + (modal && 'active')}
+      className={"Recordsmodal " + (modal ? "active" : "hidden")}
       onClick={() => setmodal(!modal)}
     >
-      <div className="Recordswrapper">
-        <div className="Record-left">
-          <div className="profile-image">
-            <ImageHelper user={user} />
-          </div>
-          <div className="info">
-            <p className="name">{user.Name}</p>
-
-            <p className="email">
-              <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon> &nbsp;
-              {user.email}
-            </p>
-            <div className="phone">
-              <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>&nbsp;
-              {user.phoneNumber}
+      <div
+        id="staticModal"
+        data-modal-backdrop="static"
+        tabIndex="-1"
+        aria-hidden="true"
+        className="fixed top-0 left-0 right-0 z-50 w-full flex justify-center items-center p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full"
+      >
+        <div className="relative w-full h-full max-w-2xl md:h-">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex overflow-auto items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Teacher's Details
+              </h3>
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-hide="staticModal"
+                onClick={() => setmodal(!modal)}
+              >
+                <svg
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
             </div>
-          </div>
-        </div>
-        <div className="Record-right">
-          <div className="personal">
-            <div className="head">Personal</div>
-            <div className="p-info">
-              <div>
-                <label>Date Of Birth: </label>
-                <span>{user.DOB}</span>
-              </div>
-              <div>
-                <label>Gender: </label>
-                <span>{user.gender}</span>
-              </div>
-              <div>
-                <label>Mother's Name: </label>
-                <span>{user.motherName}</span>
-              </div>
-              <div>
-                <label>Father's Name: </label>
-                <span>{user.fatherName}</span>
-              </div>
-              <div>
-                <label>Address: </label>
-                <span>{user.address}</span>
-              </div>
-            </div>
-          </div>
-          <div className="school">
-            <div className="head">School</div>
-            <div className="p-info">
-              <div>
-                <label>Designation: </label>
-                <span>{user.postedDesignationName}</span>
-              </div>
-              <div>
-                <label>Posted School: </label>
-                <span>{user.postedSchoolName}</span>
-              </div>
-              <div>
-                <label>Posted School Location: </label>
-                <span>{user.postedSchoolLocation}</span>
-              </div>
-              <div>
-                <label>Date Of Joining: </label>
-                <span>{user.dateOfJoining}</span>
+            <div className="w-full h-full overflow-auto">
+              <div className="w-full h-full">
+                <div className="relative overflow-x-auto">
+                  <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <tbody>
+                      <tr className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                        <td>
+                          <ImageHelper user={user} />
+                        </td>
+                        <td className="pl-3">
+                          <div className="text-base font-semibold">
+                            {user.Name}
+                          </div>
+                          <div className="font-normal text-gray-500">
+                            {user.email}
+                          </div>
+                        </td>
+                      </tr>
+                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Name
+                        </th>
+                        <td className="px-6 py-4">{user.Name}</td>
+                      </tr>
+                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Email
+                        </th>
+                        <td className="px-6 py-4">{user.email}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Phone Number
+                        </th>
+                        <td className="px-6 py-4">{user.phoneNumber}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Date Of Birth
+                        </th>
+                        <td className="px-6 py-4">{user.DOB}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Gender
+                        </th>
+                        <td className="px-6 py-4">{user.gender}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Mother's Name
+                        </th>
+                        <td className="px-6 py-4">{user.motherName}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Father's Name
+                        </th>
+                        <td className="px-6 py-4">{user.fatherName}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Address
+                        </th>
+                        <td className="px-6 py-4">{user.address}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Designation
+                        </th>
+                        <td className="px-6 py-4">
+                          {user.currentDesignationPost}
+                        </td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Posted School
+                        </th>
+                        <td className="px-6 py-4">{user.postedSchoolName}</td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Posted School Location
+                        </th>
+                        <td className="px-6 py-4">
+                          {user.postedSchoolLocation}
+                        </td>
+                      </tr>
+                      <tr className="bg-white dark:bg-gray-800">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          Date Of Joining
+                        </th>
+                        <td className="px-6 py-4">{user.dateOfJoining}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
--- client/src/pages/authorityDashboard/pages/records/Records.jsx
+++ client/src/pages/authorityDashboard/pages/records/Records.jsx
@@ -1,17 +1,14 @@
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react';
-// import './records.css';
-import ImageHelper from '../../../../component/ImageHelper';
-import RecordsModal from '../../components/modal/recordModal/recordsModal';
-import { recordsA } from '../../helper';
+import React, { useEffect, useState } from "react";
+import ImageHelper from "../../../../component/ImageHelper";
+import RecordsModal from "../../components/modal/recordModal/recordsModal";
+import { recordsA } from "../../helper";
 
 export default function Records() {
   const [users, setusers] = useState([]);
   const [modal, setmodal] = useState(false);
   const [muser, setmusers] = useState({});
-  const [search, setsearch] = useState('');
-  const keys = ['Name', 'email', 'postedDesignationName'];
+  const [search, setsearch] = useState("");
+  const keys = ["Name", "email", "postedDesignationName"];
 
   const loadAllRecords = () => {
     recordsA().then((data) => {
@@ -26,144 +23,141 @@ export default function Records() {
     loadAllRecords();
   }, []);
   return (
-    <div className="right--outer--layer w-full h-full">
-        <div className="container--box w-full h-full">
-          <div className="upper--bar">
-            <span className="upper--bar--title">Teacher's Records</span>
-            <span className="search--box">
+    <div className="right--outer--layer w-full h-full overflow-auto">
+      <div className="container--box w-full h-full">
+        <div className="upper--bar">
+          <span className="upper--bar--title">Teacher's Records</span>
+          <span className="search--box">
+            <input
+              type="text"
+              className="upper--bar--searchBox"
+              placeholder="Search by Name,email,Post"
+              onChange={(event) => {
+                setsearch(event.target.value);
+              }}
+            />
+          </span>
+        </div>
+
+        <div className="filter--box--layerTwo">
+          <div className="filter--box" id="filter--box">
+            <div className="filterbox--innerLayer">
               <input
                 type="text"
-                className="upper--bar--searchBox"
-                placeholder="Search by Name,email,Post"
-                onChange={(event) => {
-                  setsearch(event.target.value);
-                }}
+                className="field fld--one"
+                placeholder="Search by name here.."
               />
-            </span>
-            {/* <span className="upper--bar--filter">
-              <FontAwesomeIcon icon={faFilter} size="2x" />
-              <button className="upperBar--subtitle" id="toggle">
-                Filter
+              <select className="field fld--two" id="Customer">
+                <option selected>Designation</option>
+                <option value="1">HTML</option>
+                <option value="2">CSS</option>
+                <option value="3">JavaScript</option>
+              </select>
+              <label for="joiningDAte" className="filter--label labelOne">
+                Joining date
+              </label>
+              <input
+                type="date"
+                className="field fld--three"
+                id="joiningDAte"
+              />
+              <label for="FilterDob" className="filter--label labelTwo">
+                DOB
+              </label>
+              <input type="date" className="field fld--four" id="FilterDob" />
+            </div>
+
+            <div className="filterbox--innerLayerTwo">
+              <input
+                type="Text"
+                className="field fld--five"
+                id="FilterSchool"
+                placeholder="Search by school here.."
+              />
+              <label for="FilterRetrYear" className="filter--label labelThree">
+                Retirement year
+              </label>
+              <input
+                type="date"
+                className="field fld--six"
+                id="FilterRetrYear"
+              />
+              <button className="fields--btn" type="submit">
+                Apply
               </button>
-            </span> */}
-          </div>
-
-          <div className="filter--box--layerTwo">
-            <div className="filter--box" id="filter--box">
-              <div className="filterbox--innerLayer">
-                <input
-                  type="text"
-                  className="field fld--one"
-                  placeholder="Search by name here.."
-                />
-                <select className="field fld--two" id="Customer">
-                  <option selected>Designation</option>
-                  <option value="1">HTML</option>
-                  <option value="2">CSS</option>
-                  <option value="3">JavaScript</option>
-                </select>
-                <label for="joiningDAte" className="filter--label labelOne">
-                  Joining date
-                </label>
-                <input
-                  type="date"
-                  className="field fld--three"
-                  id="joiningDAte"
-                />
-                <label for="FilterDob" className="filter--label labelTwo">
-                  DOB
-                </label>
-                <input type="date" className="field fld--four" id="FilterDob" />
-              </div>
-
-              <div className="filterbox--innerLayerTwo">
-                <input
-                  type="Text"
-                  className="field fld--five"
-                  id="FilterSchool"
-                  placeholder="Search by school here.."
-                />
-                <label
-                  for="FilterRetrYear"
-                  className="filter--label labelThree"
-                >
-                  Retirement year
-                </label>
-                <input
-                  type="date"
-                  className="field fld--six"
-                  id="FilterRetrYear"
-                />
-                <button className="fields--btn" type="submit">
-                  Apply
-                </button>
-              </div>
             </div>
           </div>
-
-          <div className="table">
-            <div className="table--content--box" id="TableBox">
-              <table className="table--content">
-                <thead className="table--header">
-                  <tr className="table--row">
-                    <th className="table--title th--name">Name</th>
-                    <th className="table--title th--post">Post</th>
-                    <th className="table--title th--gender">Gender</th>
-                    <th className="table--title th--phone">Phone</th>
-                    <th className="table--title th--email">Email</th>
-                    <th className="table--title th--detail">Detail</th>
-                  </tr>
-                </thead>
-
-                <tbody className="table--body">
-                  {users
-                    .filter((user) =>
-                      keys.some((key) =>
-                        user[key].toLowerCase().includes(search)
-                      )
-                    )
-                    .map((user) => {
-                      return (
-                        <tr className="table--row" key={user._id}>
-                          <td className="tableData td--name">
-                            <ImageHelper user={user} />
-                            {user.Name}
-                          </td>
-                          <td className="tableData td--post">
-                            {user.postedDesignationName}
-                          </td>
-                          <td className="tableData td--gender">
-                            {user.gender}
-                          </td>
-                          <td className="tableData td--phone">
-                            {user.phoneNumber}
-                          </td>
-                          <td className="tableData td--email">{user.email}</td>
-                          <td className="tableData td--detail">
-                            <button
-                              className="table--btn"
-                              onClick={() => {
-                                setmodal(true);
-                                setmusers(user);
-                              }}
-                            >
-                              Detail
-                            </button>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <RecordsModal
-            onClose={() => setmodal(false)}
-            user={muser}
-            modal={modal}
-            setmodal={setmodal}
-          />
         </div>
+
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Post
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Gender
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Phone
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Detail
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {users
+                .filter((user) =>
+                  keys.some((key) => user[key].toLowerCase().includes(search))
+                )
+                .map((user) => (
+                  <tr
+                    key={user._id}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  >
+                    <th
+                      scope="row"
+                      className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      <ImageHelper user={user} />
+                      {user.Name}
+                    </th>
+                    <td className="px-6 py-4">{user.postedDesignationName}</td>
+                    <td className="px-6 py-4">{user.gender}</td>
+                    <td className="px-6 py-4">{user.phoneNumber}</td>
+                    <td className="px-6 py-4">{user.email}</td>
+                    <td className="px-6 py-4">
+                      <button
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                        onClick={() => {
+                          setmodal(true);
+                          setmusers(user);
+                        }}
+                      >
+                        Detail and Verify
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+
+        <RecordsModal
+          onClose={() => setmodal(false)}
+          user={muser}
+          modal={modal}
+          setmodal={setmodal}
+        />
       </div>
+    </div>
   );
 }
